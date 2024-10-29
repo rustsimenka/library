dce29f7228ce41cf0c1fceca1eef5a5b76de37b1
Руслан Устименко <rustsimenka@gmail.com>
2024-10-29 16:13:09 +0300

    fix: change allowed host

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-28w*vf30a_rk(wf+@p9=+$0thyjbr)n@f#blzs@t#ji3cwocij
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
