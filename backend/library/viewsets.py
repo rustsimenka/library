e903c6cd4db048a6f6923d842ae8094b96bfa9c2
Руслан Устименко <rustsimenka@gmail.com>
2024-10-30 09:47:28 +0300

    feat: add swagger

---
+++ backend/library/viewsets.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from library.models import Book
+from library.serialisers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
