39d8cfb8397acd61ffe2ba4b3b908d7336038c28
Руслан Устименко <rustsimenka@gmail.com>
2024-10-30 18:37:56 +0300

    feat: hard reset. Add count of pages.

--- backend/library/models.py
+++ backend/library/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=256)
-    count_of_pages = models.SmallAutoField()
+    count_of_pages = models.IntegerField(null=True)
 
 
