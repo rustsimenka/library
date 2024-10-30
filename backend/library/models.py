7a7a36347c6625a429af96470d82cae854f38ff4
Руслан Устименко <rustsimenka@gmail.com>
2024-10-30 17:39:48 +0300

    feat: add author in model Book

--- backend/library/models.py
+++ backend/library/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=256)
 
 class Book(models.Model):
     name = models.CharField(max_length=256)
-    count_of_pages = models.SmallAutoField()
+    count_of_pages = models.IntegerField()
+    autor = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=256)
+
