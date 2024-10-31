111bac4b01a6ecc95eecfb5e5c1402fcbf959815
Руслан Устименко <rustsimenka@gmail.com>
2024-10-31 09:30:59 +0300

    feat: add model Author

--- backend/library/models.py
+++ backend/library/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=256)
+
 
 class Book(models.Model):
     name = models.CharField(max_length=256)
+    count_of_pages = models.IntegerField(null=False)
 
