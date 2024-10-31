f36122b24a86e47eacd1aac10bee6569917668d2
Руслан Устименко <rustsimenka@gmail.com>
2024-10-31 14:25:44 +0300

    feat: return cruds in viewsets

--- backend/library/viewsets.py
+++ backend/library/viewsets.py
@@ -1,10 +1,10 @@
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from library.models import Book
 from library.serialisers import BookSerializer
 from rest_framework import mixins
 
 
-class BookViewSet(mixins.CreateModelMixin, GenericViewSet):
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
 
     serializer_class = BookSerializer
