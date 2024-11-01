732522acd379b57bfff8d0ea7c421e8d2613a830
Руслан Устименко <rustsimenka@gmail.com>
2024-11-01 16:23:56 +0300

    feat: names books and authors are uniq

--- backend/library/models.py
+++ backend/library/models.py
@@ -5,15 +5,15 @@ from django.db import models
 class ExplicitModel(models.Model):
     class Meta:
         abstract = True
+
     objects: models.Manager
 
 
 class Author(ExplicitModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
 
 
 class Book(ExplicitModel):
-    name = models.CharField(max_length=256, validators=[MinLengthValidator(4)])
+    name = models.CharField(max_length=256, validators=[MinLengthValidator(4)], unique=True)
     pages = models.IntegerField(null=False)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
